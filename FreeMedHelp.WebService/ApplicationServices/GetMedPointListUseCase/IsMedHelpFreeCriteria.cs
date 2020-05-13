1b30518373c90ef23eed26c151971d6e61cc3e31
BasanovaOA <50922999+BasanovaOA@users.noreply.github.com>
2020-05-13 23:17:58 +0300

    Add files via upload

---
+++ FreeMedHelp.WebService/ApplicationServices/GetMedPointListUseCase/IsMedHelpFreeCriteria.cs
@@ -0,0 +1,20 @@
+﻿using FreeMedHelp.DomainObjects;
+using FreeMedHelp.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace FreeMedHelp.ApplicationServices.GetMedPointListUseCase
+{
+    public class IsMedHelpFreeCriteria : ICriteria<MedPoint>
+    {
+        public string IsMedHelpFree { get; }
+
+        public IsMedHelpFreeCriteria(string ismedhelpfree)
+            => IsMedHelpFree = ismedhelpfree;
+
+        public Expression<Func<MedPoint, bool>> Filter
+            => (mp => mp.IsMedHelpFree == IsMedHelpFree);
+    }
+}
---
+++ FreeMedHelp.WebService/InfrastructureServices/Gateways/Database/MedContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FreeMedHelp.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeMedHelp.InfrastructureServices.Gateways.Database
+{
+    public class MedContext : DbContext
+    {
+        public DbSet<MedPoint> MedPoints { get; set; }
+
+        public MedContext(DbContextOptions options)
+            : base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            FillTestData(modelBuilder);
+        }
+        private void FillTestData(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<MedPoint>().HasData(
+                new
+                {
+                    Id = 1L,
+                    IsMedHelpFree = "591",
+                    FullName = @"Метро ""Войковская"" - Станция Ховрино",
+                
+                },
+                new
+                {
+                    Id = 2L,
+                    IsMedHelpFree = "191",
+                    FullName = @"Метро ""Селигерская"" - Станция Ховрино",
+
+                },
+                new
+                {
+                    Id = 3L,
+                    IsMedHelpFree = "215к",
+                    FullName = @"Метро ""Селигерская"" - Станция Ховрино",
+
+                },
+                new
+                {
+                    Id = 4L,
+                    IsMedHelpFree = "59",
+                    FullName = @"Метро ""Сокол"" - Улица Генерала Глаголева",
+ 
+                }
+            );
+        }
+    }
+}
