25a51c76c6b4fa5e06328ed6087686e596c247dd
BasanovaOA <50922999+BasanovaOA@users.noreply.github.com>
2020-05-13 21:41:27 +0300

    Add files via upload

---
+++ SharedComponents/FreeMedHelp.MedPointListQuery/ApplicationServices/GetRouteListUseCase/GetMedPointListUseCaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using FreeMedHelp.DomainObjects;
+using FreeMedHelp.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeMedHelp.ApplicationServices.GetMedPointListUseCase
+{
+    public class GetMedPointListUseCaseResponse : UseCaseResponse
+    {
+        public IEnumerable<MedPoint> MedPoints { get; }
+
+        public GetMedPointListUseCaseResponse(IEnumerable<MedPoint> medpoints) => MedPoints = medpoints;
+    }
+}
