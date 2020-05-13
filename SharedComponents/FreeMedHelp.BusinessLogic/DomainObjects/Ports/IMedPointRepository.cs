5db68b31bda5cf66b7f31284c69865d62537b249
BasanovaOA <50922999+BasanovaOA@users.noreply.github.com>
2020-05-13 21:35:28 +0300

    Add files via upload

---
+++ SharedComponents/FreeMedHelp.BusinessLogic/DomainObjects/Ports/IMedPointRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace FreeMedHelp.DomainObjects.Ports
+{
+    public interface IReadOnlyMedPointRepository
+    {
+        Task<MedPoint> GetMedPoint(long id);
+
+        Task<IEnumerable<MedPoint>> GetAllMedPoints();
+
+        Task<IEnumerable<MedPoint>> QueryMedPoints(ICriteria<MedPoint> criteria);
+
+    }
+
+    public interface IMedPointRepository
+    {
+        Task AddRoute(MedPoint route);
+
+        Task RemoveRoute(MedPoint route);
+
+        Task UpdateRoute(MedPoint route);
+    }
+}
